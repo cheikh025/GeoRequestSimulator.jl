2284e81a8bc3fa613c71bf9a3aa8ee05f8dad119
cheikh025 <cheikh.aghibou@gmail.com>
2024-02-11 22:12:30 -0500

    add build

---
+++ deps/cpp/build/CMakeFiles/GenerateScenario.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenerateScenario.dir/Block.cpp.o"
+  "CMakeFiles/GenerateScenario.dir/Block.cpp.o.d"
+  "CMakeFiles/GenerateScenario.dir/Params.cpp.o"
+  "CMakeFiles/GenerateScenario.dir/Params.cpp.o.d"
+  "CMakeFiles/GenerateScenario.dir/Point.cpp.o"
+  "CMakeFiles/GenerateScenario.dir/Point.cpp.o.d"
+  "CMakeFiles/GenerateScenario.dir/Polygon.cpp.o"
+  "CMakeFiles/GenerateScenario.dir/Polygon.cpp.o.d"
+  "CMakeFiles/GenerateScenario.dir/main.cpp.o"
+  "CMakeFiles/GenerateScenario.dir/main.cpp.o.d"
+  "libGenerateScenario.pdb"
+  "libGenerateScenario.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GenerateScenario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
