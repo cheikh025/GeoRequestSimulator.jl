993a23d896e16d8fbbab6da4b1cd87bc76d628e3
cheikh025 <cheikh.aghibou@gmail.com>
2024-02-11 22:37:52 -0500

    t

--- src/utils.jl
+++ src/utils.jl
@@ -5,7 +5,8 @@ module CppScenario
         script_dir = @__DIR__
         return joinpath(script_dir, "../deps/cpp/build/libGenerateScenario.so")
     end
-    @wrapmodule(get_libGenSC_path)
+    script_dir = @__DIR__
+    @wrapmodule(joinpath(script_dir, "../deps/cpp/build/libGenerateScenario.so"))
 
     function __init__()
         @initcxx
