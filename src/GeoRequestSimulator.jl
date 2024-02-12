b0c1ec22c992832d4187e566865b29397ee1ea07
cheikh025 <cheikh.aghibou@gmail.com>
2024-02-12 13:48:19 -0500

    fix

--- src/GeoRequestSimulator.jl
+++ src/GeoRequestSimulator.jl
@@ -1,11 +1,19 @@
 module GeoRequestSimulator
 
     include("../deps/build.jl")
-    include("utils.jl")
-    using .CppScenario: SCmain
-    
+    using CxxWrap
+    # Define a function that returns the library path
+    function get_libGenSC_path()
+        script_dir = @__DIR__
+        return joinpath(script_dir, "../deps/cpp/build/libGenerateScenario.so")
+    end
+    script_dir = @__DIR__
+    @wrapmodule(joinpath(script_dir, "../deps/cpp/build/libGenerateScenario.so"))
 
-    export createScenario
+    function __init__()
+        @initcxx
+    end
+    
     function createScenario(city, depot_location, nb_bu_large, district_size, nb_scenario)
         pathScenario = "Scenario/output/$(city)_$(depot_location)_$(nb_bu_large)_$(district_size).json"
     
@@ -14,8 +22,9 @@ module GeoRequestSimulator
         end
     
         StringInstance = city * "_C_" * string(nb_bu_large) * "_" * string(district_size) * "_" * string(nb_scenario)
-        SCmain(StringInstance, "data/", "Scenario/output")
+        GeoRequestSimulator.SCmain(StringInstance, "data/", "Scenario/output/")
     end
+    export createScenario
 
 
 end
