2c3a49a9356ed72c3b802cc9f127495c3827675e
cheikh025 <cheikh.aghibou@gmail.com>
2024-02-11 22:17:40 -0500

    test

--- src/utils.jl
+++ src/utils.jl
@@ -3,7 +3,7 @@ module CppScenario
     # Define a function that returns the library path
     function get_libGenSC_path()
         script_dir = @__DIR__
-        return joinpath(script_dir, "../deps/cpp/build/libGenerateScenario.so")
+        return joinpath(script_dir, "deps/cpp/build/libGenerateScenario.so")
     end
     CxxWrap.@wrapmodule(get_libGenSC_path)
 end
