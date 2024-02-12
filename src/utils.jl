ecc0f33dec1900b28b2b092b76b9a3e4f1cfa78c
cheikh025 <cheikh.aghibou@gmail.com>
2024-02-11 22:10:27 -0500

    test

--- src/utils.jl
+++ src/utils.jl
@@ -1,9 +1,17 @@
 module CppScenario
     using CxxWrap
-    # Define a function that returns the library path
+
+    function init_lib()
+        lib_path = get_libGenSC_path()
+        # Assuming `CxxWrap.load` is the correct way to load the library in your version of CxxWrap
+        CxxWrap.@wrapmodule(lib_path)
+        return
+    end
+
     function get_libGenSC_path()
         script_dir = @__DIR__
         return joinpath(script_dir, "../deps/cpp/build/libGenerateScenario.so")
     end
-    CxxWrap.@wrapmodule(get_libGenSC_path)
+
+    init_lib()
 end
